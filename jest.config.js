0dfb898d892ecb25afc4bf4c7b20326de3589bef
ophilar <ophilar@gmail.com>
2025-04-22 06:12:53 +0300

    refactor: Update Jest configuration to use custom test environment with global.crypto mock

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   transform: {},
-  testEnvironment: 'jsdom',
+  testEnvironment: './test/jest.env.js',
   moduleFileExtensions: ['js', 'mjs', 'json', 'node'],
   testMatch: ["**/test/**/*.test.js"],
   setupFilesAfterEnv: ["./test/setup.js"]
---
+++ test/jest.env.js
@@ -0,0 +1,12 @@
+const NodeEnvironment = require('jest-environment-jsdom');
+
+class CustomCryptoEnvironment extends NodeEnvironment {
+  async setup() {
+    await super.setup();
+    if (!this.global.crypto) {
+      this.global.crypto = { randomUUID: () => 'mock-uuid-1234' };
+    }
+  }
+}
+
+module.exports = CustomCryptoEnvironment;
